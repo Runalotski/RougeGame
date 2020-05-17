a2f5bb19d3defb02f4a8c3397d8ed8733946ae40
Adam <adam.chap@hotmail.co.uk>
2020-05-17 22:16:05 +0100

    Simple Character Controller
    Created a simple animated model that can collide with walls

---
+++ RogueGame/Assets/PlayerCharacter/Scripts/MyCharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour
+{
+    Animator anim;
+    CharacterController charController;
+
+    Vector3 lastVec = Vector3.zero;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        charController = GetComponent<CharacterController>();
+        anim.applyRootMotion = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVec = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical")).normalized;
+        charController.Move(moveVec * Time.deltaTime * speed);
+
+        if (moveVec != Vector3.zero)
+        {
+            anim.SetBool("IsMoving", true);
+            lastVec = moveVec;
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+
+        
+
+        transform.forward = lastVec;
+    }
+}
