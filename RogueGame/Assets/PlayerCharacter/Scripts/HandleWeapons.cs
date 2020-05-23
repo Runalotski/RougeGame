c72de77f7541aaa6d18971e82ac2ab85c2d187eb
Adam <adam.chap@hotmail.co.uk>
2020-05-23 02:15:36 +0100

    Added a Simple Weapon

---
+++ RogueGame/Assets/PlayerCharacter/Scripts/HandleWeapons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleWeapons : MonoBehaviour
+{
+    Transform heldWeapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            heldWeapon.GetComponent<IWeaponClass>().Attack();
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log(hit.transform.name);
+
+        Debug.Log("Component " + hit.transform.root.GetComponent<MonoBehaviour>());
+
+        if (hit.transform.root.GetComponent<MonoBehaviour>() is IWeaponClass)
+        {
+            Debug.Log("Weapon Picked up");
+            if(heldWeapon == null)
+            {
+                PickupWeapon(hit.transform.root);
+            }
+        }
+    }
+
+    void PickupWeapon(Transform weaponT)
+    {
+        heldWeapon = weaponT;
+        weaponT.parent = this.transform;
+        weaponT.position = new Vector3(0, 2, 0);
+        weaponT.rotation = Quaternion.identity;
+        weaponT.GetComponent<IWeaponClass>().owner = this.transform;
+    }
+}
---
+++ RogueGame/Assets/Weapons/Scripts/IWeaponClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeaponClass
+{
+    byte handedness { get; set; }
+
+    Transform owner { get; set; }
+
+    void Attack();
+
+    float attackSpeed { get; set; }
+}
