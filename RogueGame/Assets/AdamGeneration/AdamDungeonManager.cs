49c0d59762ed8ee22c361d9a9fc6b0cefb0f7efe
Adam <adam.chap@hotmail.co.uk>
2020-05-25 22:17:22 +0100

    Added Dungeon Gen

---
+++ RogueGame/Assets/AdamGeneration/AdamDungeonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdamDungeonManager : MonoBehaviour
+{
+    public enum TileTypes { Floor, Wall};
+
+    public static AdamDungeonData dungeonData;
+    public static AdamDungeonRenderer dungeonRenderer;
+
+    public int DungeonLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        Random.seed = (int)System.DateTime.Now.Ticks;
+
+        List<DungeonNode> test = new List<DungeonNode>();
+
+        dungeonData = new AdamDungeonData(DungeonLength);
+        dungeonRenderer = new AdamDungeonRenderer();
+
+        //dungeonRenderer.Render(dungeonData.grid);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (dungeonData != null && dungeonData.HasPath())
+        {
+            for (int i = 0; i < /*(Time.timeSinceLevelLoad * 8) % */ (dungeonData.path.Count - 1); i++)
+            {
+                if (i == 0)
+                    Gizmos.color = Color.green;
+                else
+                    Gizmos.color = Color.red;
+
+                Gizmos.DrawLine(new Vector3(dungeonData.path[i].x + 0.5f, 0, dungeonData.path[i].z + 0.5f),
+                                new Vector3(dungeonData.path[i + 1].x + 0.5f, 0, dungeonData.path[i + 1].z + 0.5f));
+
+                Gizmos.DrawSphere(new Vector3(dungeonData.path[i].x + 0.5f, 0, dungeonData.path[i].z + 0.5f), 0.2f);
+            }
+
+            Gizmos.DrawSphere(new Vector3(dungeonData.path[dungeonData.path.Count - 1].x + 0.5f, 0, dungeonData.path[dungeonData.path.Count - 1].z + 0.5f), 0.2f);
+        }
+
+    }
+}
---
+++ RogueGame/Assets/AdamGeneration/DungeonNode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonNode
+{
+    public List<DungeonNode> childNodes;
+
+    public int x;
+    public int z;
+
+    public DungeonNode(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
+    public DungeonNode(Vector2 pos)
+    {
+        this.x = (int)pos.x;
+        this.z = (int)pos.y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+            return false;
+
+        DungeonNode other = obj as DungeonNode;
+
+        // Would still want to check for null etc. first.
+        return other != null &&
+               this.x == other.x &&
+               this.z == other.z;
+    }
+    public override int GetHashCode()
+    {
+        return this.x ^ this.z;
+    }public override string ToString()
+    {
+        return "DungeionNode(" + this.x + "," + this.z + ")";
+    }
+}
