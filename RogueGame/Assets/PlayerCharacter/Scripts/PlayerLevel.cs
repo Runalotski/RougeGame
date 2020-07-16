0505df423e909150ea6a55a62d4f32c2b220f80e
Adam <adam.chap@hotmail.co.uk>
2020-07-16 23:04:12 +0100

    Did something

---
+++ RogueGame/Assets/PlayerCharacter/Scripts/PlayerLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour
+{
+    public int Level { get; private set; }
+
+    public ulong Xp { get; private set; }
+
+    public ulong NextLevelXP
+    {
+        get
+        { 
+            return (ulong)((Level + 1) * (Level + 1)) * 100;
+        }
+    }
+
+    public void AddXP(int xp)
+    {
+        Xp += (ulong)xp;
+
+        while (Xp >= NextLevelXP)
+        {
+            Level++;
+        }
+    }
+}
