007e789bf47cee333daca6d2fe49ea9a858d9088
Adam <adam.chap@hotmail.co.uk>
2020-08-05 17:51:58 +0100

    Added a health Bar

---
+++ RogueGame/Assets/UI/HealthBarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    public Text healthText;
+
+    public Slider healthSlider;
+
+    public Actor player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = player.health + " / " + player.maxHealth;
+
+        healthSlider.value = player.health / player.maxHealth;
+    }
+}
